976984d40a68215e624aa3227a483b3852a41448
perry385817606 <385817606@qq.com>
2018-04-03 21:57:04 +0800

    过滤了ip.txt中重复的值
    过滤了ip.txt中重复的值

--- py01_查找excel数据/find_excel_data.py
+++ py01_查找excel数据/find_excel_data.py
@@ -39,12 +39,15 @@ def filter_assets(data, info_list, field):
     
     for k, v in data.items():
         assets_list = v['assets_list']
-        sheet_data = {'assets_list': []}
+        # sheet_data = {'assets_list': []}
+        tmp_data = []
         for asset in assets_list:
-            for info in  info_list:
-                if asset.get(field) == info:
-                    sheet_data['assets_list'].append(asset)
-                    filter_data[k] = { 'headers': v['headers'],'assets_list': sheet_data.get('assets_list') }
+            for info in info_list:
+                if asset.get(field) == info and info:    # 去掉空字符
+                    # sheet_data['assets_list'].append(asset)
+                    # filter_data[k] = { 'headers': v['headers'],'assets_list': sheet_data.get('assets_list') }
+                    tmp_data.append(asset)
+                    filter_data[k] = { 'headers': v['headers'],'assets_list': tmp_data }
     return filter_data
 
 
@@ -69,16 +72,17 @@ def write_excel(filter_data, Sheets, output='output.xls'):
     try:
         wb.save(output)
     except:
-        print('文件写入失败!')
+        print('%s 写入失败!' % output)
     else:
-        print('文件写入成功!')
+        print('%s 写入成功!' % output)
 
 
 def get_msg(file_name):
     info_list = []
     with open(file_name, encoding='utf-8') as f:
         info_list = [ line.strip() for line in f ]
-    return info_list
+
+    return list(set(info_list))   # 去掉ip.txt中重复的数据
 
 
 def no_found(field, info_list, filter_data):
@@ -88,16 +92,29 @@ def no_found(field, info_list, filter_data):
         for item in assets_list:
             found.append(item.get(field))
     
-
-    no_found = (set(info_list).difference(set(found)) )
+    no_found = (set(info_list).difference(set(found)))
     return list(no_found)
 
 
+def wirte_file(info, outfile):
+    if info:
+        try:
+            with open(outfile, 'w', encoding='utf-8') as f:
+                for line in info:
+                    f.write('%s\n' % str(line))
+        except:
+            print('%s 写入失败!' % outfile)
+        else:
+            print('%s 写入成功!' % outfile)
+    else:
+        print('没有未查找到的信息.')
+
+
 if __name__ == '__main__':
-    field = u'名称'
+    field = u'DCN IP'
     info_list = get_msg('ip.txt')
     file_name = 'ip.txt'
-    excelFile = u'test.xlsx'
+    excelFile = u'test.xls'
 
     Sheets = get_sheets(excelFile)
     data = get_data(excelFile, Sheets)
@@ -105,4 +122,6 @@ if __name__ == '__main__':
     write_excel(filter_data, Sheets)
     no_found = no_found(field, info_list, filter_data)
 
-    print('no found....\n',no_found)
+    # print('no found....\n',no_found)
+
+    wirte_file(no_found, 'no_found.txt')
